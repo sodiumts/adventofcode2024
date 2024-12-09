4d63b871266c6b8419882ebe59a247d640f6b736
sodiumts <ernests.malnacs@gmail.com>
2024-12-09 10:17:20 +0100

    Part1 done

--- day9/part1.cpp
+++ day9/part1.cpp
@@ -8,6 +8,8 @@
 #include <utility>
 #include <unordered_map>
 #include <unordered_set>
+#include <queue>
+#include <algorithm>
 
 struct pair_hash {
     template <typename T1, typename T2>
@@ -18,12 +20,96 @@ struct pair_hash {
     }
 };
 
+std::queue<int> getFreeSpaces(const std::vector<int> &diskMap) {
+    std::queue<int> freeSpaces;
+    bool isFreeSpace = false;
+    int bigIndex = 0;
+    for (int i = 0; i < diskMap.size(); i++) {
+        if(!isFreeSpace) {
+            bigIndex += diskMap[i];
+            isFreeSpace = true;
+        } else {
+            for (int j = 0; j < diskMap[i]; j++) {
+                freeSpaces.push(bigIndex + j);
+            }
+            bigIndex += diskMap[i];
+            isFreeSpace = false;
+        }
+    }
+    return freeSpaces;
+}
 
+bool hasGap(const std::vector<int> &disk) {
+    bool hadOne = false;
+    for (int i = 0; i < disk.size(); i++) {
+        if(disk[i] == -1) {
+            hadOne = true;
+        }
+        if(hadOne && disk[i] != -1) {
+            return true;
+        }
+    }
+    return false;
 
+}
+
+std::queue<int> getFreeSpaces2(const std::vector<int> &disk) {
+    std::queue<int> freeSpaces;
+
+    for (int i = 0; i < disk.size(); i++) {
+        if(disk[i] == -1) {
+            freeSpaces.push(i);
+        }   
+    }
+    return freeSpaces;
+}
+
+uint64_t calculateChecksum(const std::vector<int> &diskMap) {
+    auto freeSpaces = getFreeSpaces(diskMap);
+    
+    std::vector<int> diskNums;
+    bool isFile = true;
+    int fileIDCounter = 0;
+    for (int i = 0; i < diskMap.size(); i++) {
+       if(isFile) {
+            for (int j = 0; j < diskMap[i]; j++) {
+                diskNums.push_back(fileIDCounter);
+            }
+            fileIDCounter++;
+            isFile = false;
+       } else {
+            for (int j = 0; j < diskMap[i]; j++) {
+                diskNums.push_back(-1);
+            }
+            isFile = true;
+       }
+    }
+    std::vector<int> freeSpace;
+    for(int i = diskNums.size() - 1; i >= 0; i--) {
+        if(diskNums[i] != -1 && freeSpaces.size() > 0) {
+            diskNums[freeSpaces.front()] = diskNums[i];
+            freeSpaces.pop();
+            diskNums[i] = -1;
+            if(hasGap(diskNums)) {
+                freeSpaces = getFreeSpaces2(diskNums);
+            } else {
+                break;
+            }
+        } 
+    }
+    uint64_t checksum = 0;
+    for (int i = 0; i < diskNums.size(); i++) {
+        if(diskNums[i] == -1) {
+            break;
+        }
+        checksum += i * diskNums[i];
+    }
+    return checksum;
+}
 int main() {
     auto start = std::chrono::high_resolution_clock::now();
 
-    std::ifstream file("test.txt");
+    std::ifstream file("input.txt");
     if (!file.is_open()) {
         throw std::runtime_error("Failed to open file");
     }
@@ -38,12 +124,7 @@ int main() {
         } 
     }
 
-    for (auto c: diskNum) {
-        std::print("{}", c);
-    }
-    std::println();
-
-    //std::println("Resonance Point Count: {}", );
+    std::println("Checksum: {}", calculateChecksum(diskNum)); 
     
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
